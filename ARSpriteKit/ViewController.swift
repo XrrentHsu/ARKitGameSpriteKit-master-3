1959c1129f0636d36954db20b626446b7afcb1e9
User18 <user18@user18de-imac.local>
2019-01-14 11:49:28 +0800

    Initial Commit

---
+++ ARSpriteKit/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  ARSpriteKit
+//
+//  Created by Esteban Herrera on 7/4/17.
+//  Copyright © 2017 Esteban Herrera. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import ARKit
+
+class ViewController: UIViewController, ARSKViewDelegate {
+    
+    @IBOutlet var sceneView: ARSKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        let scene = Scene(size: sceneView.bounds.size)
+        scene.scaleMode = .resizeFill
+        sceneView.presentScene(scene)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func randomInt(min: Int, max: Int) -> Int {
+        return min + Int(arc4random_uniform(UInt32(max - min + 1)))
+    }
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        let ghostId = randomInt(min: 1, max: 8)
+        let node = SKSpriteNode(imageNamed: "ghost\(ghostId)")
+        node.name = "ghost"
+        return node
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+    }
+}
